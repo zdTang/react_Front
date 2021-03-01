db3d3c46cefc0c03bd30d0a7294c318f24bb6cf8
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2021-03-01 07:38:39 -0500

    release token

---
+++ src/config/redditToken.js
@@ -0,0 +1,17 @@
+ const token={
+
+userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36',
+clientId: 'h8PCl1ykdWxcAA',
+clientSecret: 'TCmMJItd9icxcePzJ-CgRe-1TXPCUw',
+username: 'mike-tang',
+password: 'Frontend123'
+
+}
+
+const ApiOption={
+    HotPostNumber: 9   // the number of posts
+
+}
+
+
+export {token,ApiOption}
